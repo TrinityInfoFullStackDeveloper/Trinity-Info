f1ad9b62aff1614be685f45e75d294ad25a39daa
TrinityInfoFullStackDeveloper <76439154+TrinityInfoFullStackDeveloper@users.noreply.github.com>
2020-12-21 23:58:04 +0530

    Add files via upload

---
+++ phplib/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Trinity Info | ONE OF THE FASTEST GROWING IT SERVICES COMPANY IN INDIA</title>
+    <meta name="description"
+        content="Trinity Info is a top web & mobile app development company. We offer custom web & mobile design & development on market demand services.">
+    <meta name="keywords"
+        content="Web design company, eCommerce website development company in coimbatore,  best web development company in coimbatore, web development company coimbatore, 
+web application development company in coimbatore, best web design company in coimbatore, web design company jobs in coimbatore, digital marketing company in coimbatore, digital marketing company coimbatore, tamil nadu,
+web development company in tamilnadu, website creation company, website design company in india, online website development company, website development company in india,
+website design india price, low price website design in india, low cost website design company, ecommerce website development company in coimbatore, ecommerce website development, ecommerce website development services, ecommerce website design services, ecommerce website development india">
+    <meta name="author" content="Mareeswaran">
+    <meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Trinity Info - Software Company - We are a nimble team of developers ..." />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#322d97">
+
+    <link href="images/favicon.png" rel="icon">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/plugin.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+</head>
---
+++ phplib/scripts.php
@@ -0,0 +1,6 @@
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/plugin.min.js"></script>
+    <script src="js/main.js"></script>
